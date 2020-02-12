ccb22e33c8a41c25562ee61b58fd0f458db04e8f
AquaDevelopers <myaquaapps@gmail.com>
2020-02-12 11:15:29 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/u/aquadirectory/precache-manifest.c21919ebb6c3fd9009741a38b87bb005.js"
+  "/precache-manifest.319204f388baf49443c0e0ee969d9609.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/u/aquadirectory/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
