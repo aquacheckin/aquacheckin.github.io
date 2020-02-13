975f8431dd2d29562721db955e789171909cb9a5
AquaDevelopers <myaquaapps@gmail.com>
2020-02-13 07:34:04 -0600

    Add files via upload

---
+++ precache-manifest.3defa2ef308b12a49ae589ecc2afc905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f50480d0409f2f48756",
+    "url": "/static/css/main.c942a570.chunk.css"
+  },
+  {
+    "revision": "9f50480d0409f2f48756",
+    "url": "/static/js/main.8cc57598.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fda1f15021cff79c47c9",
+    "url": "/static/js/2.5211740d.chunk.js"
+  },
+  {
+    "revision": "d9cbc3fc1e94ec44d990bb6c2e396f29",
+    "url": "/static/media/Roll2Screen_elImage_62945.d9cbc3fc.png"
+  },
+  {
+    "revision": "0eb118837007116930347dc036dbd6bb",
+    "url": "/static/media/WaitScreen_elImage_986857.0eb11883.png"
+  },
+  {
+    "revision": "a27d82a2fa98113f572e6c9aed48203d",
+    "url": "/static/media/StartScreen_elImage2_267274.a27d82a2.jpg"
+  },
+  {
+    "revision": "1050ab2dc354c422f97ceb3421b9a7b3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b80ad119ea0a472bf0cd6f314d72b8.js"
+  "/precache-manifest.3defa2ef308b12a49ae589ecc2afc905.js"
 );
 
 workbox.clientsClaim();
