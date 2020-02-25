ecdf7cb4d190747dc4e0a9d330279f2940004026
AquaDevelopers <myaquaapps@gmail.com>
2020-02-25 10:20:16 -0600

    Update mobilemsg.js

--- mobilemsg.js
+++ mobilemsg.js
@@ -1,10 +1,7 @@
-window.onload=function(){
-    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-    if (mobile) {
-        window.onorientationchange = function() { 
-  if(window.innerHeight > window.innerWidth){
+window.norientationchange = function() { 
+      if(window.innerHeight > window.innerWidth){
+           window.location.replace('demo.html');
 }else{
  window.location.replace('index.html');
 }
 };
-  }
