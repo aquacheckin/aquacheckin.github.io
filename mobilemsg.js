6c62ecdc5e98436b96b5cfa1fff56a9fc984c207
AquaDevelopers <myaquaapps@gmail.com>
2020-02-25 10:00:34 -0600

    Update mobilemsg.js

--- mobilemsg.js
+++ mobilemsg.js
@@ -2,9 +2,16 @@ window.onload=function(){
     var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
     if (mobile) {
         if(window.innerHeight > window.innerWidth){
+    
     alert("Please rotate phone to landscape mode for best experience");  
 }            
     } else {
 
     }
   }
+
+window.onorientationchange = function() { 
+  if(window.innerHeight > window.innerWidth){
+    alert("Please rotate phone to landscape mode for best experience");  
+}        
+};
