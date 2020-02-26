dc1a6b094e6104ebe3f41534bae7970921c6fb35
AquaDevelopers <myaquaapps@gmail.com>
2020-02-26 06:10:26 -0600

    Update mobilemsg.js

--- mobilemsg.js
+++ mobilemsg.js
@@ -1,3 +1,22 @@
-if(window.innerHeight > window.innerWidth){
-    alert("You must rotate device to use");
+// Get HTML head element 
+var head = document.getElementsByTagName('HEAD')[0];  
+  
+// Create new link Element 
+var link = document.createElement('link'); 
+  
+// set the attributes for link element  
+link.rel = 'stylesheet';  
+      
+link.type = 'text/css'; 
+      
+link.href = 'lock.css';  
+  
+if ((navigator.userAgent.match(/iPhone/)) || (navigator.userAgent.match(/iPod/))) {
+// Append link element to HTML head 
+head.appendChild(link);  
+}
+
+if (navigator.userAgent.match(/Android/)) {
+// Append link element to HTML head 
+head.appendChild(link);  
 }
